3a3e5bd7a4e645e451ce3d47e7f9668f4a0f4d57
manuelKrivoy <krivoymanuel@gmail.com>
2025-10-15 17:30:27 -0300

    ui

--- fe/turnos-app/src/main.jsx
+++ fe/turnos-app/src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./App.css";
+import "./App.css"; // contiene @import "tailwindcss"
+import "./styles/ui.css"; // tus clases con @apply (sin volver a importar Tailwind)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
